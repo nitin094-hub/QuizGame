b26aeeb3b755ea5c84272eb661963a22a2855bc2
Dhritiman Bharadwaj <dhritimanbhardwaj@gmail.com>
2022-01-25 10:15:50 +0530

    Created Question Serializer

--- UserQuizzes/serializers.py
+++ UserQuizzes/serializers.py
@@ -1,7 +1,13 @@
+from attr import fields
 from rest_framework import serializers
-from .models import Quizzes
+from .models import Questions, Quizzes
 
 class QuizSerializer(serializers.ModelSerializer):
     class Meta:
         model = Quizzes
+        fields = "__all__"
+
+class QuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Questions
         fields = "__all__"
