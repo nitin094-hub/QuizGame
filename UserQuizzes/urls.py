ff1619c0cb3d453770b09cf2d09370ffe0ee1081
Dhritiman Bharadwaj <dhritimanbhardwaj@gmail.com>
2022-01-27 12:09:23 +0530

    Added Url For UserScores

--- UserQuizzes/urls.py
+++ UserQuizzes/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from UserQuizzes.views import QuestionsAPI, QuestionsListCreateAPIView, QuizAPI, QuizListCreateAPIView
+from UserQuizzes.views import QuestionsAPI, QuestionsListCreateAPIView, QuizAPI, QuizListCreateAPIView, ScoreAPIView
 
 urlpatterns = [
     path('quiz/',QuizListCreateAPIView.as_view()),
     path('quiz/<uuid:pk>',QuizAPI.as_view()),
     path('questions/',QuestionsListCreateAPIView.as_view()),
     path('questions/<int:pk>',QuestionsAPI.as_view()),
+    path('scores/',ScoreAPIView.as_view()),
 ]
